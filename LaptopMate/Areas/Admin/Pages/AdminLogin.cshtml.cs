6f94d816a1d23df35426c9edc75f81e8fcb9058b
Manav <manavkasare725@gmail.com>
2023-09-28 14:46:53 +0530

    added admin button to navbar

---
+++ LaptopMate/Areas/Admin/Pages/AdminLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LaptopMate.Areas.Admin.Pages
+{
+    public class AdminLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
